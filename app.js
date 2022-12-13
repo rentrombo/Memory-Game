b99458d5432a9e0bd15ed6b5c814ef2f297cc728
Rowen Trombo <119641186+rentrombo@users.noreply.github.com>
2022-12-13 17:53:26 -0500

    rn
    Signed-off-by: Rowen Trombo <119641186+rentrombo@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,64 @@
+const cardArray = [
+    {
+        name: 'pride',
+        img: 'images/pride.jpg' 
+    },
+    {
+        name: 'america',
+        img: 'images/america.jpg' 
+    },
+    {
+        name: 'loveislove',
+        img: 'images/loveislove.jpg' 
+    },
+    {
+        name: 'turkey',
+        img: 'images/turkey.jpg' 
+    },
+    {
+        name: 'england',
+        img: 'images/england.jpg' 
+    },
+    {
+        name: 'mexico',
+        img: 'images/mexico.jpg' 
+    },
+    {
+        name: 'pride',
+        img: 'images/pride.jpg' 
+    },
+    {
+        name: 'america',
+        img: 'images/america.jpg' 
+    },
+    {
+        name: 'loveislove',
+        img: 'images/loveislove.jpg' 
+    },
+    {
+        name: 'turkey',
+        img: 'images/turkey.jpg' 
+    },
+    {
+        name: 'england',
+        img: 'images/england.jpg' 
+    },
+    {
+        name: 'mexico',
+        img: 'images/mexico.jpg' 
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+
+
+function createBoard(){
+    for (let i=0; i < 10; i++){
+       const card = document.createElement('img')
+       card.setAttribute('src', 'images/card-back.jpg')
+       card.setAttribute('data-id', i)
+       gridDisplay.appendChild(card)
+    }
+}
